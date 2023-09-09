98470f6941b4252794f19b32e01ddeb5e0c03e9f
Christian Gonzales <124095890+cdgonzo23@users.noreply.github.com>
2023-09-09 14:18:51 -0700

    built out portfolio section and added images to the card background

--- src/components/pages/Projects.jsx
+++ src/components/pages/Projects.jsx
@@ -1,6 +1,25 @@
+import Portfolio from '../utils/projects'
+
 function Projects() {
   return (
+    <>
     <h2>Projects</h2>
+    <ul>
+      {Portfolio.map((project) => (
+      <div className='card text-white bg-primary mb-3' key={project.id}>
+        <div className='card-header'>{project.title}</div>
+        <div style={{backgroundImage: `url(${project.src})`}}>
+          {/* <img src={project.src} /> */}
+          <ul>
+            <a href={project.link}>Deployed App</a>
+            <a href={project.github}>GitHub</a>
+          </ul>
+        </div>
+      </div>
+      ))}
+    </ul>
+    </>
+
   )
 }
 
---
+++ src/components/utils/projects.jsx
@@ -0,0 +1,53 @@
+import StateQuiz from '../../assets/state-quiz.png';
+import TechBlog from '../../assets/tech-blog.png';
+import MainMan from '../../assets/maintenance-manatee.png';
+import JATE from '../../assets/J-A-T-E.png';
+import WeatherDashboard from '../../assets/weather-dashboard.png';
+import NightInGen from '../../assets/night-in-generator.png';
+
+const projectArray = [
+  {
+    id: "1",
+    title: "Maintenance Manatee",
+    src: MainMan,
+    link: "https://maintenance-manatee-20230807-e071f072b462.herokuapp.com/",
+    github: "https://github.com/cdgonzo23/maintenance-manatee"
+  },
+  {
+    id: "2",
+    title: "Night-In Generator",
+    src: NightInGen,
+    link: "https://cdgonzo23.github.io/night-in-generator/",
+    github: "https://github.com/cdgonzo23/night-in-generator"
+  },
+  {
+    id: "3",
+    title: "Tech Blog",
+    src: TechBlog,
+    link: "https://mvc-tech-blog-20230802-f118edb4f2ae.herokuapp.com/",
+    github: "https://github.com/cdgonzo23/tech-blog"
+  },
+  {
+    id: "4",
+    title: "PWA Text Editor",
+    src: JATE,
+    link: "https://another-text-editor-040923-3c59dc8eb62b.herokuapp.com/",
+    github: "https://github.com/cdgonzo23/pwa-text-editor"
+  },
+  {
+    id: "5",
+    title: "State Capital Challenge",
+    src: StateQuiz,
+    link: "https://cdgonzo23.github.io/code-quiz/",
+    github: "https://github.com/cdgonzo23/code-quiz"
+  },
+  {
+    id: "6",
+    title: "Weather Dashboard",
+    src: WeatherDashboard,
+    link: "https://cdgonzo23.github.io/weather-dashboard/",
+    github: "https://github.com/cdgonzo23/weather-dashboard"
+  }
+]
+
+export default projectArray;
